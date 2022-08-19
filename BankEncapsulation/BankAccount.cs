16e3db4c003533161d64fc2a47074cf7df4110fc
Cody <Cojo341@gmail.com>
2022-08-18 20:57:24 -0400

    Initial Commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double Balance;
+        public BankAccount(double balance)
+        {
+            Balance = balance;
+        }
+
+        public  void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public double GetBalance()
+        {
+            return Balance;
+            throw new NotImplementedException();
+
+        }
+
+
+    }
+
+    public class MoneyMoves
+    {
+        public static bool CheckIfDone(string ans)
+        {
+            ans.ToLower();
+            while (true)
+            {
+                ;
+                if (ans != null && (ans == "y" || ans == "yes"))
+                {
+                    return true;
+
+                }
+                else if (ans != null && (ans == "n" || ans == "no"))
+                {
+                    return false;
+
+                }
+                else
+                {
+                    Console.WriteLine("Only yes/no or y/n Allowed: ");
+                    ans = Console.ReadLine();
+                }
+            }
+        }
+
+
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,34 @@
+﻿using BankEncapsulation;
+using System.Collections.Generic;
+
+
+double depositAmount;
+Console.WriteLine("Hello would you like to make a deposit?");
+Console.WriteLine("Only yes/no or y/n Allowed: ");
+bool keepGoing = MoneyMoves.CheckIfDone(Console.ReadLine());
+var yourAccount = new BankAccount(0);
+
+while (keepGoing)
+{ 
+    Console.WriteLine("Please enter the ammount you would like to deposit");
+
+    var nullCheck = double.TryParse(Console.ReadLine(), out depositAmount);
+
+    while (!nullCheck)
+    {
+        Console.WriteLine("Please enter the ammount you would like to deposit");
+        nullCheck = double.TryParse(Console.ReadLine(), out depositAmount);
+    }
+
+    yourAccount.Deposit(depositAmount);
+
+    Console.WriteLine($"Your New Balance is: {yourAccount.GetBalance()} ");
+
+    Console.WriteLine("Would You Like to Make another Deposit?");
+    Console.WriteLine("Only yes/no or y/n Allowed: ");
+
+    keepGoing = MoneyMoves.CheckIfDone(Console.ReadLine());
+
+}
+
+Console.WriteLine("See you next time!");
